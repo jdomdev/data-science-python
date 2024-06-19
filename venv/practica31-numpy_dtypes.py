1f8693f7283591d9eeec8f787880ca7e45b46234
john-sunday <jdomingo1010@duck.com>
2024-06-19 21:14:52 +0200

    [EN PROCESO] Práctica 31 - Numpy dtypes(página 12)

--- venv/practica31-numpy_dtypes.py
+++ venv/practica31-numpy_dtypes.py
@@ -1,9 +1,13 @@
 # A Comprehensive Guide to NumPy Data Types
 # What else is out there besides int32 and float64?
+
 import numpy as np
 print(np.__version__) # test
 
-# 1.Integers
+
+# 1.INTEGERS
+# ----------
+
 
 np.array([1,2,3]).dtype
 # dtype('int32')
@@ -50,6 +54,77 @@ with np.errstate(over='raise'):
 with np.errstate(over='ignore'):
     print(np.array([2**31-1])[0]+1)
 # -2147483648
-import numpy as np
+
 a = np.array([10], dtype=object)
 len(str(a**1000))
+# 1003
+
+
+# 2. FLOATS
+# ---------
+
+
+x = np.array([-1234.5])
+1/(1+np.exp(-x))
+# <stdin>:1: RuntimeWarning: overflow encountered in exp
+# array([0.])
+
+np.exp(np.array([1234.5]))
+# array([inf])
+
+x = np.array([-1234.5], dtype=np.float128)
+1/(1+np.exp(-x))
+# array([0.]) # -> float128: no disponible en Windows
+
+9279945539648888.0+1
+# = 9279945539648888.0
+
+len('9279945539648888')
+# = 16
+
+from decimal import Decimal as D
+a = np.array([D('0.1'), D('0.2')]); a
+# = array([Decimal('0.1'), Decimal('0.2')], dtype=object)
+a.sum()
+# = Decimal('0.3')
+
+from fractions import Fraction
+a = np.array([Fraction(1, 10), Fraction(1, 5)], dtype=object)
+# = a = np.array([Fraction(1, 10), Fraction(1, 5)], dtype=object)
+a.sum()
+# = Fraction(3, 10)
+
+np.array([1+2j])
+# = array([1.+2.j])
+
+a = np.array([np.nan, 5, np.nan, 5, 5])
+a[~np.isnan(a)].mean()
+# = 5.0
+
+
+# 3. BOOLS
+# --------
+
+import sys
+sys.getsizeof(True)
+# = 28
+
+
+# 4. CADENAS
+# ----------
+
+
+np.array(['abcde', 'x', 'y', 'x'])
+# = array(['abcde', 'x', 'y', 'x'], dtype='<U5')
+
+
+
+
+
+
+
+
+
+
+
+
